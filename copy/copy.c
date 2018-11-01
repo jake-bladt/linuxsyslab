4404cf4c9cceb4d03a1d9e6dd81ccc6747d745ca
Jake Bladt <jake@jakebladt.com>
2018-11-01 14:11:47 +0000

    check for arg count

--- copy/copy.c
+++ copy/copy.c
@@ -6,4 +6,16 @@
 #define BUF_SIZE 1024
 #endif
 
+int main(int argc, *argv[])
+{
+  int inputFd, outputFd, openFlags;
+  mode_t filePerms;
+  char buf[BUF_SIZS];
+
+  if(argc != 3 || strcmp(argv[1], "--help") == 0)
+    usageErr("%s old-file new-file\n", argv[0]);
+
+  exit(EXIT_SUCCESSS);
+
+}
 
